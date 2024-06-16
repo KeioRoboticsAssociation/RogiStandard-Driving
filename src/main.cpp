926c003789aa0638c2a22d8ed826de4f5936c870
dragoemon2 <matsuoka_ryunosuke@keio.jp>
2024-06-16 21:01:48 +0900

    mainをきれいにした

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#if 0
+// includeする
 #include <mbed.h>
 #include "Encoder/Encoder.hpp"
 #include "OdomWheel/Odometry.hpp"
@@ -7,26 +7,30 @@
 #include "pins.hpp"
 #include "Simulator/MotorSimulator.hpp"
 
+// encoderの設定
 Encoder encoder1(InterruptInPins::MEASURING_ENCODER1_A, DigitalInPins::MEASURING_ENCODER1_B, 2048, 0, false);
 Encoder encoder2(InterruptInPins::MEASURING_ENCODER2_A, DigitalInPins::MEASURING_ENCODER2_B, 2048, 0, false);
 Encoder encoder3(InterruptInPins::MEASURING_ENCODER3_A, DigitalInPins::MEASURING_ENCODER3_B, 2048, 0, false);
 
+// DCモータの設定
 DCMotor dc1(PwmOutPins::MOTOR1_PWM, DigitalOutPins::MOTOR1_DIR, 0);
 DCMotor dc2(PwmOutPins::MOTOR2_PWM, DigitalOutPins::MOTOR2_DIR, 0);
 DCMotor dc3(PwmOutPins::MOTOR3_PWM, DigitalOutPins::MOTOR3_DIR, 0);
 
-MotorController motor1(dc1, encoder1, PIDGain({0.1, 0.01, 0.01, 100}));
-MotorController motor2(dc2, encoder2, PIDGain({0.1, 0.01, 0.01, 100}));
-MotorController motor3(dc3, encoder3, PIDGain({0.1, 0.01, 0.01, 100}));
+// モーターコントローラの設定
+MotorController motor1(dc1, encoder1);
+MotorController motor2(dc2, encoder2);
+MotorController motor3(dc3, encoder3);
 
-float WHEEL_RAD = 100.0;
-float TREAD_RAD = 100.0;
+// オムニホイールの配置を設定
+float WHEEL_RAD = 100.0; // 車輪の半径
+float TREAD_RAD = 100.0; // 中心から車輪までの距離
 std::array<WheelConfig, 3> config = {
     WheelConfig{
-        .wheel_radius = WHEEL_RAD, 
-        .wheel_x = 0.0, 
-        .wheel_y = TREAD_RAD, 
-        .wheel_theta = M_PI
+        .wheel_radius = WHEEL_RAD, // 車輪の半径
+        .wheel_x = 0.0, // 車輪のx座標
+        .wheel_y = TREAD_RAD,  // 車輪のy座標
+        .wheel_theta = M_PI // 車輪の角度
     }, 
     WheelConfig{
         .wheel_radius = WHEEL_RAD, 
@@ -42,116 +46,12 @@ std::array<WheelConfig, 3> config = {
     }
 };
 
+// オドメトリとホイールコントローラの設定
 Odometry<3> odometry(config, {&encoder1, &encoder2, &encoder3});
 WheelController<3> controller(config, {&motor1, &motor2, &motor3});
 
-
-
 int main() {
-    controller.setTargetTwist({1000.0, 0.0, 0.0});
-    odometry.setPose({0.0,0.0,0.0});
-    while (1) {
-        int a = int(60 * motor1.getTargetSpeed());
-        int b = int(60 * motor2.getTargetSpeed());
-        int c = int(60 * motor3.getTargetSpeed());
-        printf("%d, %d, %d\n", a, b, c);
-        ThisThread::sleep_for(100ms);
-        encoder1.addCount(0);
-        encoder2.addCount(0);
-        encoder3.addCount(0);
-        auto pose = odometry.getPose();
-        //printf("%d\n", odometry.DEBUG_);
-        //printf("%d, %d, %d\n", encoder1.getCount(), encoder2.getCount(), encoder3.getCount());
-        printf("x: %d, y: %d, theta: %d\n", int(1000 * pose.x), int(1000 * pose.y), int(1000 * pose.theta));
-    }
+    // ここにプログラムを書く。
 }
 
 
-#endif
-
-#if 0
-
-#include <mbed.h>
-#include "OdomWheel/Odometry.hpp"
-
-Encoder encoder1(D2, D3, 2048, 0, false);
-Encoder encoder2(D4, D5, 2048, 0, false);
-
-std::array<WheelConfig, 2> config = {
-    WheelConfig{100.0, 100.0, 0.0, M_PI / 4}, 
-    WheelConfig{100.0, -100.0, 0.0, 2 * M_PI / 4}
-};
-
-TwoWheelOdometry odometry(config, {&encoder1, &encoder2});
-
-int main() {
-    
-}
-
-
-
-
-#endif
-
-
-#include <mbed.h>
-#include "Encoder/Encoder.hpp"
-#include "OdomWheel/Odometry.hpp"
-#include "OdomWheel/WheelController.hpp"
-#include "DCMotor/DCMotor.hpp"
-#include "pins.hpp"
-#include "Simulator/MotorSimulator.hpp"
-#include "OdomWheel/OdomWheel.hpp"
-
-Encoder encoder_mecha_r(InterruptInPin::ENC_MECHA_R_A, InterruptInPin::ENC_MECHA_R_B, 2048, 0, false);
-Encoder encoder_mecha_l(InterruptInPin::ENC_MECHA_L_A, InterruptInPin::ENC_MECHA_L_B, 2048, 1, false);
-Encoder encoder_omni(InterruptInPin::ENC_OMNI_A, InterruptInPin::ENC_OMNI_B, 2048, 0, false);
-
-DCMotor dc_mecha_r(PwmOutPin::PWM_MECHA_R, DigitalOutPin::DIR_MECHA_R, 1);
-DCMotor dc_mecha_l(PwmOutPin::PWM_MECHA_L, DigitalOutPin::DIR_MECHA_L, 0);
-DCMotor dc_omni(PwmOutPin::PWM_OMNI, DigitalOutPin::DIR_OMNI, 1);
-
-// MotorController motor_mecha_r(dc_mecha_r, encoder_mecha_r);
-// MotorController motor_mecha_l(dc_mecha_l, encoder_mecha_l);
-// MotorController motor_omni(dc_omni, encoder_omni);
-
-// OdomWheel<3> odom_wheel(
-//     {
-//         WheelConfig{
-//             .wheel_radius = 100.0,
-//             .wheel_x = 100.0,
-//             .wheel_y = 100.0,
-//             .wheel_theta = 0.0
-//         },
-//         WheelConfig{
-//             .wheel_radius = 100.0,
-//             .wheel_x = -100.0,
-//             .wheel_y = 100.0,
-//             .wheel_theta = M_PI / 2
-//         },
-//         WheelConfig{
-//             .wheel_radius = 100.0,
-//             .wheel_x = 0.0,
-//             .wheel_y = -100.0,
-//             .wheel_theta = M_PI
-//         }
-//     },
-//     {&motor_mecha_r, &motor_mecha_l, &motor_omni},
-//     {&encoder_mecha_r, &encoder_mecha_l, &encoder_omni}
-// );
-
-
-int main() {
-    //DigitalOut dir(PC_14);
-    // motor_mecha_r.setTargetSpeed(1.0);
-    // motor_mecha_l.setTargetSpeed(1.0);
-    // motor_omni.setTargetSpeed(1.0);
-    //dc_mecha_l.setDuty(-0.2);
-    //dc_mecha_r.setDuty(-0.2);
-    dc_omni.setDuty(-0.2);
-
-    while (1) {
-        printf("%d, %d, %d\n", encoder_mecha_r.getCount(), encoder_mecha_l.getCount(), encoder_omni.getCount());
-        ThisThread::sleep_for(100ms);
-    }
-}
--- src/pins.hpp
+++ src/pins.hpp
@@ -1,127 +1,52 @@
-// #pragma once
-// #include <PinNames.h>
-
-// namespace DigitalOutPins
-// {
-//     constexpr PinName MOTOR1_DIR = PA_4; // モータ1の方向
-//     constexpr PinName MOTOR2_DIR = PC_14; // モータ2の方向
-//     constexpr PinName MOTOR3_DIR = PA_7; // モータ3の方向
-// }
-
-// namespace PwmOutPins
-// {
-//     constexpr PinName MOTOR1_PWM = PA_11; // モータ1のPWM
-//     constexpr PinName MOTOR2_PWM = PA_9; // モータ2のPWM
-//     constexpr PinName MOTOR3_PWM = PA_10; // モータ3のPWM
-// }
-
-// namespace DigitalInPins
-// {
-//     constexpr PinName MEASURING_ENCODER1_B = PC_0; // 測定輪エンコーダ1のB相
-//     constexpr PinName MEASURING_ENCODER2_B = PC_1; // 測定輪エンコーダ2のB相
-//     constexpr PinName MEASURING_ENCODER3_B = PB_2; // 測定輪エンコーダ3のB相
-// }
-
-// namespace InterruptInPins
-// {
-//     constexpr PinName MEASURING_ENCODER1_A = PB_1; // 測定輪エンコーダ1のA相
-//     constexpr PinName MEASURING_ENCODER2_A = PA_12; // 測定輪エンコーダ2のA相
-//     constexpr PinName MEASURING_ENCODER3_A = PB_1; // 測定輪エンコーダ3のA相
-// }
-
-
-
-// namespace AnalogInPins
-// {
-    
-// }
-
-// namespace AnalogOutPins
-// {
-    
-// }
-
-// namespace UartPins
-// {
-    
-// }
-
-// namespace I2cPins
-// {
-    
-// }
-
 #pragma once
-
 #include <PinNames.h>
 
-namespace DigitalOutPin
+namespace DigitalOutPins
 {
-    constexpr PinName DIR_MECHA_R = PA_4; // md1
-    constexpr PinName DIR_MECHA_L = PB_7; // md2 kawatta
-    constexpr PinName DIR_OMNI = PB_5; // md3 kawatta
+    constexpr PinName MOTOR1_DIR = PA_4; // モータ1の方向
+    constexpr PinName MOTOR2_DIR = PC_14; // モータ2の方向
+    constexpr PinName MOTOR3_DIR = PA_7; // モータ3の方向
 }
 
-namespace DigitalInPin
+namespace PwmOutPins
+{
+    constexpr PinName MOTOR1_PWM = PA_11; // モータ1のPWM
+    constexpr PinName MOTOR2_PWM = PA_9; // モータ2のPWM
+    constexpr PinName MOTOR3_PWM = PA_10; // モータ3のPWM
+}
+
+namespace DigitalInPins
+{
+    constexpr PinName MEASURING_ENCODER1_B = PC_0; // 測定輪エンコーダ1のB相
+    constexpr PinName MEASURING_ENCODER2_B = PC_1; // 測定輪エンコーダ2のB相
+    constexpr PinName MEASURING_ENCODER3_B = PB_2; // 測定輪エンコーダ3のB相
+}
+
+namespace InterruptInPins
+{
+    constexpr PinName MEASURING_ENCODER1_A = PB_1; // 測定輪エンコーダ1のA相
+    constexpr PinName MEASURING_ENCODER2_A = PA_12; // 測定輪エンコーダ2のA相
+    constexpr PinName MEASURING_ENCODER3_A = PB_1; // 測定輪エンコーダ3のA相
+}
+
+
+
+namespace AnalogInPins
 {
     
 }
 
-namespace PwmOutPin
+namespace AnalogOutPins
 {
-    // Timer1
-    constexpr PinName PWM_MECHA_R = PA_11; // md1
-    constexpr PinName PWM_MECHA_L = PA_10; // md2
-    constexpr PinName PWM_OMNI = PA_9; // md3
-
-    // Timer3
-    constexpr PinName PWM_ESC = PB_6;
-
-    constexpr PinName PWM_TANK_R = PC_9; //servo1
-    constexpr PinName PWM_TANK_C = PC_8; //servo2
-    constexpr PinName PWM_TANK_L = PA_6; //servo3
     
-    constexpr PinName PWM_SERVO_BASE = PB_2; //servo4
-    constexpr PinName PWM_SERVO_TIP = PB_8; //servo5
-    constexpr PinName PWM_SERVO_BLDC = PB_9;    //servo6
-} 
+}
 
-namespace InterruptInPin
+namespace UartPins
 {
-    //enc 1
-    constexpr PinName ENC_MECHA_R_A = PB_1;
-    constexpr PinName ENC_MECHA_R_B = PC_0;
-
-    //enc 2
-    constexpr PinName ENC_MECHA_L_A = PA_12;
-    constexpr PinName ENC_MECHA_L_B = PC_1;
-
-    //enc 3
-    constexpr PinName ENC_OMNI_A = PB_13;
-    constexpr PinName ENC_OMNI_B = PC_11;
-
-    //enc 4
-    constexpr PinName OdomR_A = PB_14;
-    constexpr PinName OdomR_B = PC_10;
     
-    //enc5
-    constexpr PinName OdomL_A = PB_15;
-    constexpr PinName OdomL_B = PC_7;
 }
 
-namespace UARTPin{
-    constexpr PinName UART_TX = PA_1;
-    constexpr PinName UART_RX = PA_0;
-}
-
-namespace I2CPin{
-    constexpr PinName I2C2_SDA = PC_12;
-    constexpr PinName I2C2_SCL = PB_10;
-
-    constexpr PinName I2C3_SDA = PB_4;
-    constexpr PinName I2C3_SCL = PB_10;
-}
-
-namespace AnalogInPin {
-    constexpr PinName TOF = PB_0;
+namespace I2cPins
+{
+    
 }
